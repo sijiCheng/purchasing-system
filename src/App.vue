7f999245ac4f776eae975d4b04e2eace36ca743c
csj001745 <940932074@qq.com>
2020-04-30 17:49:48 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <mt-search
+                v-model="value"
+               :placeholder="$t('system.searchInputPlaceholderText')"
+               :cancel-text="$t('system.cancelButtonText')"></mt-search>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data:function () {
+    return{
+      value:'11111111'
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import './normalize.css'
+import i18n from './i18n';
 
+Vue.use(MintUI)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  i18n
 }).$mount('#app')
